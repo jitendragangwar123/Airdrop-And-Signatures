2bd340976cc9dabc28e52bf8555b2f3be5fd38f1
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2025-02-12 00:02:42 +0530

    fix:deployment script

--- script/DeployMerkleAirdrop.s.sol
+++ script/DeployMerkleAirdrop.s.sol
@@ -10,7 +10,7 @@ contract DeployMerkleAirdrop is Script {
     bytes32 public ROOT = 0x51326eca4720e5a2669b827c49e333e0e586a60bfc91e33ec94872a4e0289a56;
     uint256 public AMOUNT_TO_TRANSFER = 4 * (25 * 1e18);
 
-    function deployMerkleAirdrop() public returns (MerkleAirdrop, BFTToken) {
+    function run() public returns (MerkleAirdrop, BFTToken) {
         vm.startBroadcast();
         BFTToken bftToken = new BFTToken();
         MerkleAirdrop airdrop = new MerkleAirdrop(ROOT, IERC20(bftToken));
@@ -18,9 +18,5 @@ contract DeployMerkleAirdrop is Script {
         IERC20(bftToken).transfer(address(airdrop), AMOUNT_TO_TRANSFER);
         vm.stopBroadcast();
         return (airdrop, bftToken);
-    }
-
-    function run() external returns (MerkleAirdrop, BFTToken) {
-        return deployMerkleAirdrop();
     }
 }
